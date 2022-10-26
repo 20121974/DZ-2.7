a92cb6a851371cdc8de97077e0134bbe6e60f2b6
20121974 <nfi46102@mail.ru>
2022-10-26 08:20:08 +0300

    2 commit

---
+++ src/SupermarketCheckout.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SupermarketCheckout<T> {
+    private Queue<String> queue = new LinkedList<>(List.of("Иванов", "Петров", "Сидоров", "Налев", "Котов"));
+
+    public void addBuyer(T buyer) {//добавляем покупателя в очередь
+        Object buyer1;
+        if (equals(buyer buyer1) {
+        queue.offer((String) buyer);}
+    }
+
+
+    private Queue<String> queue1 = new LinkedList<>(List.of("Смирнов", "Хвостов", "Крюков", "Углов", "Пэт"));
+
+    public void addBuyer1(T buyer1) {//добавляем покупателя в очередь
+        queue.offer((String) buyer1);
+    }
+
+
+}
